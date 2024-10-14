331377d56eddd41d5367de6762ae214f3a8e4bd6
Alex Bates <>
2024-10-14 23:49:00 +0100

    fix: page height

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-slate-100 dark:bg-slate-900 font-mono">
     <Header />
-    <main>
+    <main class="min-h-[calc(100dvh_-_150px)]">
       <slot />
     </main>
     <Footer />
