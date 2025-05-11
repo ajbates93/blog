de1de17191ec9645975cd3ab9c3c24b669f53c09
Alex Bates <>
2025-05-11 21:58:44 +0100

    feat: add title and blog

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="p-5 flex items-center justify-end gap-5 text-lg sm:mb-10">
+  <div class="p-5 sm:px-10 flex items-center justify-between gap-5 text-lg sm:mb-10 container mx-auto">
+    <nav class="flex gap-2">
+      <NuxtLink to="/">home</NuxtLink>
+      <span>|</span>
+      <NuxtLink to="/blog">blog</NuxtLink>
+    </nav>
     <UIcon class="cursor-pointer" @click="toggleTheme" :name="themeIcon" />
   </div>
 </template>
---
+++ app/pages/blog/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <section
+    class="container mx-auto p-5 sm:p-10 text-left"
+  >
+    <Blog />
+
+  </section>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
